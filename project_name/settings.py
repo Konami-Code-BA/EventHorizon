7e7de6e14cfb22b3a2152e2c7dd398e91402db2c
michaels234 <mdsimeone@gmail.com>
2021-05-22 12:53:36 +0900

    prepping for heroku

--- project_name/settings.py
+++ project_name/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import django_heroku
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(j7(p96vyw7r)*)8%u++g8qkkz!v=bwzymi!7a39#py&1dvt(w'
+## SECURITY WARNING: keep the secret key used in production secret!
+#SECRET_KEY = 'django-insecure-(j7(p96vyw7r)*)8%u++g8qkkz!v=bwzymi!7a39#py&1dvt(w'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+## SECURITY WARNING: don't run with debug turned on in production!
+#DEBUG = True
 
-ALLOWED_HOSTS = []
+DEBUG = False
+SECRET_KEY = config('SECRET_KEY')
+ALLOWED_HOSTS = ['event-horizon-jp.herokuapp.com/', 'localhost']
 
 
 # Application definition
