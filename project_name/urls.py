101125342d2a91d48cb156d2eb6bb6ad145ffc60
michaels234 <mdsimeone@gmail.com>
2021-05-24 19:07:48 +0900

    a

--- project_name/urls.py
+++ project_name/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from .routers import router
 from app_name.views import example
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(f'api/', include(router.urls)),
-		path('webhook/', example)
+		path('webhook/', example),
+    path("", TemplateView.as_view(template_name="application.html"), name="app"),
 ]
