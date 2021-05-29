34d62427cabe670bddd92fbde86545a832b9f199
michaels234 <mdsimeone@gmail.com>
2021-05-29 18:09:56 +0900

    this might finally work brah

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,58 +1,51 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <button v-on:click.prevent="line_thing()">line_thing</button><br>
+		<br>
+		<a href="https://lin.ee/UeSvNxR"><img height="36" border="0" src="https://scdn.line-apps.com/n/line_add_friends/btn/ja.png"></a>
   </div>
 </template>
-
 <script>
+import axios from 'axios'
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  data () {
+    return {
+			token: 'QHyTosat3st1hTca9MII4ZT8zAAfEmCSRkE7JpRFN8vXz2YcUFKbOnvr2ItzKihjBqSo2L+St2o2awCJuR9ZYhBF2zmhZTq02wUDV1JrlPtJdI9zEGBYHtlPEza+Yjrg96ldnJHNx560asXkXKIEpQdB04t89/1O/w1cDnyilFU=',
+      input_username: 'mdsimeone',
+      username: 'mdsimeone',
+			client_secret: 'f5ba1cafa7a7057e68360d4d260827f6',
+			client_id: '1655871760',
+    }
+  },
+  methods: {
+    async getuser () {
+      await axios
+        .get('http://127.0.0.1:8000/api/user/?search=' + this.input_username)
+        .then(response => (this.username = response.data[0].username))
+        .catch(error => (console.log(error)))
+      console.log(this.username)
+    },
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+    async postuser () {
+      await axios
+        .post('http://127.0.0.1:8000/api/user/', {
+          username: this.input_username,
+          password: '123'
+        })
+        .then(response => (this.username = response.data.username))
+        .catch(error => (console.log(error)))
+      console.log(this.username)
+    },
+
+    async line_thing () {
+      await axios
+        .post('http://127.0.0.1:8000/api/line_thing/', {command: 'consumption'})
+        .then(response => {console.log('response: ', response)})
+        .catch(error => {console.log('error: ', error)})
+    },
+  } // methods
+} // export
+</script>
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
--- project_name/urls.py
+++ project_name/urls.py
@@ -2,12 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from .routers import router
 from app_name.views import example
-from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+from app_name.views import index
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(f'api/', include(router.urls)),
 		path('webhook/', example),
-    path("", TemplateView.as_view(template_name="application.html"), name="app"),
+    path('', index, name='index'),
 ]
+#] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
