5b87a017a7e271449a997a61a4cc1904558340b1
michaels234 <mdsimeone@gmail.com>
2021-06-18 09:04:52 +0900

    allow admin cuz you have to login anyway. api only allowed to admin user. refresh should work

--- project_name/urls.py
+++ project_name/urls.py
@@ -8,14 +8,16 @@ from app_name.views import index
 
 
 urlpatterns = [
+    path(f'api/', include(router.urls)),
+    path('admin/', admin.site.urls),
 		path('webhook/', example),
     path('', index, name='index'),
     path('registration', index, name='index'),
+    path('login', index, name='index'),
+    path('pageTwo', index, name='index'),
 #]
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-if settings.DEBUG:
-	urlpatterns += [
-    path('admin/', admin.site.urls),
-    path(f'api/', include(router.urls))
-	]
+#if settings.DEBUG :
+#	urlpatterns += [
+#	]
