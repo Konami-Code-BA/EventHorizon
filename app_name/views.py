7bb9af5aa645e6cf2f955da660036ca42a090cd9
michaels234 <mdsimeone@gmail.com>
2021-06-27 19:43:06 +0900

    req.__dict__

--- app_name/views.py
+++ app_name/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
 import json
+import requests
 	
 def index(request):
     return render(request, template_name='index.html')
@@ -10,5 +11,11 @@ def index(request):
 @require_POST
 @csrf_exempt
 def example(request):
-	print('This is the webhook request.', list(request.POST.items()))
+	#req = requests.Request('POST','http://stackoverflow.com',headers={'X-Custom':'Test'},data='a=1&b=2')
+	#prepared = req.prepare()
+	req = request
+	print('This is the webhook request 4.', req.__dict__)
+	#print('This is the webhook request 1.', list(req.headers))
+	#print('This is the webhook request 2.', list(req.POST))
+	#print('This is the webhook request 3.', list(req.body))
 	return HttpResponse('This is the webhook response.')
