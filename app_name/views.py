259869fc8792edad9f8b97eda63961ab452d6eef
michaels234 <mdsimeone@gmail.com>
2021-06-27 20:48:30 +0900

    using get_messages

--- app_name/views.py
+++ app_name/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.messages import get_messages
 import json
 import requests
 	
@@ -13,8 +14,10 @@ def index(request):
 def example(request):
 	#req = requests.Request('POST','http://stackoverflow.com',headers={'X-Custom':'Test'},data='a=1&b=2')
 	#prepared = req.prepare()
-	req = request
-	print('This is the webhook request 4.', req.__dict__)
+	#req = request
+	for message in get_messages(request):
+		print('This is a message:', message.message)
+	#print('This is the webhook request 4.', req.__dict__)
 	#print('This is the webhook request 1.', list(req.headers))
 	#print('This is the webhook request 2.', list(req.POST))
 	#print('This is the webhook request 3.', list(req.body))
