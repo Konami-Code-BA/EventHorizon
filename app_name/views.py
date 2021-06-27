9a99746c0b36d629d0ed083003d1f24889ae2e4c
michaels234 <mdsimeone@gmail.com>
2021-06-27 14:33:26 +0900

    added @csrf_exempt

--- app_name/views.py
+++ app_name/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
-
-# Create your views here.
-
-
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
 	
 def index(request):
     return render(request, template_name='index.html')
 
 @require_POST
+@csrf_exempt
 def example(request):
 	print('This is the webhook request.', request)
 	return HttpResponse('This is the webhook response.')
