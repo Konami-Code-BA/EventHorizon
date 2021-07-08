a8258d25c6370a8e61fdff7e04c26c792c11f7db
michaels234 <mdsimeone@gmail.com>
2021-07-09 07:34:21 +0900

    try new create_user function

--- app_name/models.py
+++ app_name/models.py
@@ -2,6 +2,30 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib import admin
 from django.conf import settings
+from django.contrib.auth.models import BaseUserManager
+from django.apps import apps
+from django.contrib.auth.hashers import make_password
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, username, email, password, **extra_fields):
+        """
+        Create and save a user with the given username, email, and password.
+        """
+        if not username:
+            raise ValueError('The given username must be set')
+        email = self.normalize_email(email)
+        # Lookup the real model class from the global app registry so this
+        # manager method can be used in migrations. This is fine because
+        # managers are by definition working on the real model.
+        GlobalUserModel = apps.get_model(self.model._meta.app_label, self.model._meta.object_name)
+        username = GlobalUserModel.normalize_username(username)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractUser):
@@ -22,6 +46,7 @@ class User(AbstractUser):
 	ip_date = models.CharField(max_length=40, default='', blank=True)
 	#username = models.CharField(max_length=40, default='', unique=False, blank=False)
 
+	objects = UserManager()
 
 
 class UserAdmin(admin.ModelAdmin):
