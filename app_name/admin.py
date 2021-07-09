1605e478bddf85001137c4dfc5f8f2a1c0019a65
michaels234 <mdsimeone@gmail.com>
2021-07-10 08:03:43 +0900

    registration thru line or email should both work now

--- app_name/admin.py
+++ app_name/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-#from django.contrib.auth.admin import UserAdmin
-from .models import User, UserAdmin
+from .models import User, UserAdmin, GroupAdmin
+from django.contrib.auth.models import Group
 
 
 admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
+admin.site.register(Group, GroupAdmin)
