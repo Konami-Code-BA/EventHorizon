ad467751cc2d4717ccb4890ba5fa907aa44bd7a9
michaels234 <mdsimeone@gmail.com>
2021-07-09 23:18:42 +0900

    everything should be working for register by email now

--- app_name/viewsets.py
+++ app_name/viewsets.py
@@ -5,17 +5,14 @@ from rest_framework.response import Response
 import requests
 import json
 from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth.models import Group
 from django.contrib import auth
 from django.conf import settings
 from django.contrib.sessions.models import Session
 from decouple import config
 from django.core.mail import send_mail
-from app_name.functions import get_return_queryset
-from app_name.functions import api_to_line
+from app_name.functions import get_return_queryset, api_to_line, new_user_from_request
 from django.http import HttpResponse
 import secrets
-from django.contrib.auth.hashers import make_password
 
 
 class UserViewset(viewsets.ModelViewSet):
@@ -84,30 +81,9 @@ class UserViewset(viewsets.ModelViewSet):
 		return get_return_queryset(self, request, pk=request.user.pk)
 
 	def register_by_email(self, request):
-		display_name = request.data['display_name']
-		email = request.data['email']
-		password = request.data['password']
-		language = request.data['language']
-		random_secret = SecretsViewset.retrieve(SecretsViewset, None, 'random_secret').content.decode("utf-8")
-		print('made it registration 3')
-		user = self.model.objects.create_user()
-		user.display_name = display_name
-		user.email = email
-		user.password = make_password(password)
-		user.do_get_emails = True
-		user.language = language
-		user.is_superuser = False
-		user.is_staff = False
-		print('random_secret', random_secret)
-		user.random_secret = random_secret
-		print('made it registration 4')
-		user.save()
-		print('made it registration 5')
-		group = Group.objects.get(name='User')
-		group.user_set.add(user)
-		print('made it registration 6')
+		user = new_user_from_request(request)
 		self.login(request)
-		print('made it registration 7')
+		print('REGISTERED AND LOGGED IN')
 		return user
 
 	#def register_by_line(self, request):
@@ -129,7 +105,6 @@ class UserViewset(viewsets.ModelViewSet):
 	def login(self, request):
 		user = auth.authenticate(request)
 		if user:
-			print('login', user)
 			auth.login(request, user)
 		return user
 
@@ -153,7 +128,6 @@ class LineViewset(viewsets.ViewSet):
 	queryset = []
 	def create(self, request):
 		response = eval(f"self.{request.data['command']}(request)")
-		print('MADE IT 10')
 		return Response(response)
 
 	def consumption(self, request):
@@ -187,54 +161,37 @@ class LineViewset(viewsets.ViewSet):
 			'client_secret': config('LOGIN_CHANNEL_SECRET'),
 		}
 		response =  api_to_line('getAccessToken', params=params)
-		print("getAccessToken RESPONSE", response)
 		line_access_token = response['access_token']
 		line_refresh_token = response['refresh_token']
-		print("access_token, refresh_token", line_access_token, line_refresh_token)
 		params = {
 			'access_token': line_access_token,
 		}
 		response = api_to_line('profile', params=params)
-		print("profile RESPONSE", response)
 		line_id = response['userId']
-		print('MADE IT 1')
 		display_name = response['displayName']
-		print('MADE IT 2')
 		random_secret = SecretsViewset.retrieve(SecretsViewset, None, 'random_secret')
-		print('MADE IT 3')
 		try:
-			print('MADE IT 4')
 			user = User.objects.get(line_id=line_id)
 			user.line_access_token = line_access_token
 			user.line_refresh_token = line_refresh_token
 			user.language = language
 			user.random_secret = random_secret
-			print('MADE IT 5')
 			if user.do_get_line_display_name:
 				user.display_name = display_name
-			print('MADE IT 6')
 			user.save()
-			print('MADE IT 7')
 			request.data['line_id'] = line_id
 			request.user = user
-			print('MADE IT 8')
 		except User.DoesNotExist:
-			print('MADE IT 4b')
 			user = User.objects.create_user(
 				display_name=display_name, line_id=line_id, line_access_token=line_access_token, do_get_lines=True,
 				username='USER', language=language, is_superuser=False, is_staff=False, random_secret=random_secret,
 			)
-			print('MADE IT 5b')
 			user.save()
-			print('MADE IT 6b')
 			group = Group.objects.get(name='User')
 			group.user_set.add(user)
-			print('MADE IT 7b')
 			request.data['line_id'] = line_id
 			request.user = user
-			print('MADE IT 8b')
 		UserViewset.login(UserViewset, request)
-		print('MADE IT 9')
 		
 	def verify(self, request):
 		user = User.objects.get(pk=request.user.pk)
