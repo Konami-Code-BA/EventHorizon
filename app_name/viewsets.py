32cfafef4d0b5be8177eb01cb829459cb6ec59e7
michaels234 <mdsimeone@gmail.com>
2021-07-25 21:20:25 +0900

    line webhooks

--- app_name/viewsets.py
+++ app_name/viewsets.py
@@ -189,6 +189,7 @@ class UserViewset(viewsets.ModelViewSet):
 			if user.groups.filter(id=5).exists():  # if this user is a temp line friend
 				user.groups.clear()  # clear temp line friend group
 				user.groups.add(2)  # change to user
+			print('changing temp line friend to user')
 			user = verify_update_line_info(request, user)  # verify validity of current line data and put new data
 		except User.DoesNotExist:  # if there was no user with this id, turn visitor into user & add info
 			user = self.model.objects.get(pk=request.user.pk)  # get visitor account (already logged in)
