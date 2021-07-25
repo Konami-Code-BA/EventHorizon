9144be7a7cae24165f9ba3d7c599e94edc2db815
michaels234 <mdsimeone@gmail.com>
2021-07-25 11:34:44 +0900

    fixing line webhooks

--- app_name/views.py
+++ app_name/views.py
@@ -13,80 +13,13 @@ def index(request):
 
 @require_POST
 @csrf_exempt
-def line_webhook(request):
+def line_webhook(request):  # https://developers.line.biz/en/reference/messaging-api/#message-event
 	line_body = json.loads(request.body.decode('utf-8'))
-	received_message_1 = {  # https://developers.line.biz/en/reference/messaging-api/#message-event
-		'destination': 'Ub480d7e5ff2b8357eb196ed6729bd689',
-		'events': [
-			{
-				'type': 'message',
-				'message': {
-					'type': 'text',
-					'id': '14296801432826',
-					'text': 'status'
-				},
-				'timestamp': 1624799029188,
-				'source': {
-					'type': 'user',
-					'userId': 'U09e3b108910c1711d2732a8b9ac8a19d'
-				},
-				'replyToken': '3ef6d67db3304eebab8588ce2eff6331',
-				'mode': 'active'
-			}
-		]
-	}
-	received_message_2 = {
-		'destination': 'Ub480d7e5ff2b8357eb196ed6729bd689',
-		'events': [{
-			'type': 'message',
-			'message': {
-				'type': 'text',
-				'id': '14302576500342',
-				'text': '.bot status'
-			},
-			'timestamp': 1624886647207,
-			'source': {
-				'type': 'room',
-				'roomId': 'R243cbfb03168e95081d5895c2f99a4b5',
-				'userId': 'U09e3b108910c1711d2732a8b9ac8a19d'
-			},
-			'replyToken': 'c16418c3cc2a4aab82ab961a024f2ea8',
-			'mode': 'active'
-		}]
-	}
-	new_follower_1 = {
-		'destination': 'Ub480d7e5ff2b8357eb196ed6729bd689',
-		'events': [{
-			'type': 'follow',
-			'timestamp': 1624799586678,
-			'source': {
-				'type': 'user',
-				'userId': 'U09e3b108910c1711d2732a8b9ac8a19d'
-			},
-			'replyToken': 'be6008d01ab84e1a8ee7699102927f00',
-			'mode': 'active'
-		}]
-	}
-	lost_follower_1 = {
-		'destination': 'Ub480d7e5ff2b8357eb196ed6729bd689',
-		'events': [{
-			'type': 'unfollow',
-			'timestamp': 1624799291429,
-			'source': {
-				'type': 'user',
-				'userId': 'U09e3b108910c1711d2732a8b9ac8a19d'
-			},
-			'mode': 'active'
-		}]
-	}
-
-	#line_body = received_message_1
-	#line_body = received_message_2
-	#line_body = new_follower_1
-	#line_body = lost_follower_1
-
+	print('line_body', line_body)
 	replyToken, reply = line_bot(line_body)
 	response = "Don't need to send a reply"
+	print('replyToken', replyToken)
+	print('reply', reply)
 	if replyToken and reply:
 		url = 'https://api.line.me/v2/bot/message/reply'
 		headers = {
@@ -101,4 +34,5 @@ def line_webhook(request):
 			}]
 		})
 		response = requests.post(url, headers=headers, data=data)
+	print('response', response)
 	return HttpResponse(response)
