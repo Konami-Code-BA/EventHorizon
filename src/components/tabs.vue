a06cec39741e636ec67e646ada01816cb75bdde9
michaels234 <mdsimeone@gmail.com>
2021-08-05 22:49:37 +0900

    front page is looking great! making calendar and its also looking great!

---
+++ src/components/tabs.vue
@@ -0,0 +1,41 @@
+<template>
+	<div class="tabs">
+		<div v-for="no in tabNo">
+			<div>
+				<slot :name="no"/>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+	export default {
+		name: 'appFooter',
+		data () {
+			return {
+			}
+		},
+		components: {
+		},
+		props: {
+			tabNo: {},
+		},
+		computed: {
+		},
+		async mounted () {
+		},
+		methods: {
+		}
+	}
+</script>
+<style scoped>
+	.tabs {
+		width: 100%;
+		display: flex;
+		flex-direction: row;
+		align-items: flex-end;
+		justify-content: space-around;
+		height: 30px;
+		background-color: rgba(0, 0, 0, .2);
+		box-shadow: 0 0 1px rgba(255, 255, 255, .5);
+	}
+</style>
