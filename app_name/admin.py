915b9487622b9b15b0f4992cebc55dacf7e0ed4f
michaels234 <mdsimeone@gmail.com>
2021-12-28 23:14:22 +0900

    can now upload and save files using s3. learning how to get the files back now lol. i think i got it in the backend but not sending to the front end correctly yet. tho im not sure. in back end im getting streamingbody. if thats right im not sure but...

--- app_name/admin.py
+++ app_name/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import User, Alert, Event, Image
-from .models import UserAdmin, AlertAdmin, EventAdmin, ImageAdmin
+from .models import UserAdmin, AlertAdmin, EventAdmin
 from .models import GroupAdmin, SessionAdmin
 from django.contrib.auth.models import Group
 from django.contrib.sessions.models import Session
@@ -12,4 +12,4 @@ admin.site.unregister(Group)
 admin.site.register(Group, GroupAdmin)
 admin.site.register(Session, SessionAdmin)
 admin.site.register(Event, EventAdmin)
-admin.site.register(Image, ImageAdmin)
+admin.site.register(Image)
