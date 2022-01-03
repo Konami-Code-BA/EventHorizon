33326f7a7e9360fd8f6dcb51da04f40962990336
michaels234 <mdsimeone@gmail.com>
2022-01-03 23:19:13 +0900

    after literally a million different things, i believe ive fixed all the problems with the maps marker icons. if they show up on the test environment on the pc, but still don't show up in the webview, then its an issue with webview.

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -56,9 +56,7 @@ const webpackConfig = merge(baseWebpackConfig, {
         // Compress extracted CSS. We are using this plugin so that possible
         // duplicated CSS from different components can be deduped.
         new OptimizeCSSPlugin({
-            cssProcessorOptions: config.build.productionSourceMap ?
-                { safe: true, map: { inline: false } } :
-                { safe: true }
+            cssProcessorOptions: config.build.productionSourceMap ? { safe: true, map: { inline: false } } : { safe: true }
         }),
         // generate dist index.html with correct asset hash for caching.
         // you can customize output by editing /index.html
@@ -113,9 +111,12 @@ const webpackConfig = merge(baseWebpackConfig, {
 
         // copy custom static assets
         new CopyWebpackPlugin([{
-            from: path.resolve(__dirname, '../static'),
+            //    from: path.resolve(__dirname, '../static'),
+            //    to: config.build.assetsSubDirectory,
+            //    ignore: ['.*']
+            //}, {
+            from: path.resolve(__dirname, '../src/assets/mapIcons'),
             to: config.build.assetsSubDirectory,
-            ignore: ['.*']
         }])
     ]
 })
