88191942074b25a88cc294b08b457c9495f74f18
michaels234 <mdsimeone@gmail.com>
2022-01-21 19:00:29 +0900

    added faq page

---
+++ src/views/faq.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="main">
+		FAQ
+	</div>
+</template>
+<script>
+	import translations from '@/functions/translations.js'
+	export default {
+		name: 'faq',
+		data () {
+			return {
+			}
+		},
+		mounted () {
+		},
+		methods: {
+			t (w) { return translations.t(w) },
+		} // methods
+	} // export
+</script>
+<style scoped>
+</style>
