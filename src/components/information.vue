b999f44f81d21f012e3a8ed4307f61cef34d6785
Louis Yasuhiro <lyasuhir@gmail.com>
2022-02-09 21:59:02 +0900

    styled the help pop up

--- src/components/information.vue
+++ src/components/information.vue
@@ -11,66 +11,58 @@
 				<p>{{t('MINE')}}{{`:\xa0\xa0\xa0\xa0`}}{{t('EVENTS I AM AFFILIATED WITH')}}</p>
 				<p>{{t('PEOPLE I FOLLOW')}}{{`:\xa0\xa0\xa0\xa0`}}{{t('COMING SOON')}}</p>
 			</div>
-			
+
 			<!--events-->
 			<div v-if="whichInfo === 'events'" style="display: flex; flex-direction: column; align-items: center;">
-				<h2 style="text-align: center">{{t('EVENT TYPES')}}</h2>
+				<h2 style="text-align: center; font-size: 20px; margin: 0.4em;">{{t('EVENT TYPES')}}</h2>
 				<div style="display: flex; flex-direction: column; justify-content: space-between;
 						align-items: center; height: 300px;">
 					<div style="display: flex; flex-direction: column; justify-content: space-between;
 							align-items: center; height: 100%;">
 						<div style="display: flex; flex-direction: column;
 								align-items: center;">
-							<div style="font-size: 18px; border-bottom: solid 1px; width: 100%; text-align: center;">
+							<div class="sub-title">
 								{{t('invited2')}}
 							</div>
-							<div style="display: flex; flex-direction: row;
-									align-items: center;">
+							<div class="nav-items">
 								<img src="@/assets/myMapIcon.png" style="width: 25px; height: 25px;"/>
 								{{t('UPCOMING')}}
 							</div>
-							<div style="display: flex; flex-direction: row;
-									align-items: center;">
+							<div class="nav-items">
 								<img src="@/assets/myPastMapIcon.png" style="width: 25px; height: 25px;"/>
 								{{t('PAST')}}
 							</div>
 						</div>
 						<div style="display: flex; flex-direction: column;
 								align-items: center">
-							<div style="font-size: 18px; border-bottom: solid 1px; margin-top: 10px; width: 100%;
-									text-align: center;">
+							<div class="sub-title">
 								{{t('PUBLIC')}}
 							</div>
-							<div style="display: flex; flex-direction: row;
-									align-items: center;">
+							<div class="nav-items">
 								<img src="@/assets/publicMapIcon.png" style="width: 25px; height: 25px;"/>
 								{{t('UPCOMING')}}
 							</div>
-							<div style="display: flex; flex-direction: row;
-									align-items: center;">
+							<div class="nav-items">
 								<img src="@/assets/publicPastMapIcon.png" style="width: 25px; height: 25px;"/>
 								{{t('PAST')}}
 							</div>
 						</div>
 						<div style="display: flex; flex-direction: column;
 								align-items: center">
-							<div style="font-size: 18px; border-bottom: solid 1px; margin-top: 10px; width: 100%;
-									text-align: center;">
+							<div class="sub-title">
 								{{t('PRIVATE')}}
 							</div>
-							<div style="display: flex; flex-direction: row;
-									align-items: center;">
+							<div class="nav-items">
 								<img src="@/assets/privateMapIcon.png" style="width: 25px; height: 25px;"/>
 								{{t('UPCOMING')}}
 							</div>
-							<div style="display: flex; flex-direction: row;
-									align-items: center;">
+							<div class="nav-items">
 								<img src="@/assets/privatePastMapIcon.png" style="width: 25px; height: 25px;"/>
 								{{t('PAST')}}
 							</div>
 						</div>
 					</div>
-					<div style="width: 100%; white-space: pre-line;">
+					<div style="width: 100%; white-space: pre-line; font-size:0.8em; margin-bottom: 0.5em;">
 						{{ t('Note: Private events\' locations aren\'t shown in the exact place.') }}
 					</div>
 				</div>
@@ -79,8 +71,8 @@
 			<!--FINISH INFO-->
 
 			<div class="line-height"/>
-			
-			<div style="display: flex; flex-direction: row;">
+
+			<div style="display: flex; flex-direction: row; font-size:0.8em;">
 				{{ t('For more info, go to the') }}
 				<button v-on:click.prevent="goToFaq()" class="link-button">
 					FAQ
@@ -121,4 +113,18 @@
 	}
 </script>
 <style scoped>
+
+.sub-title {
+  font-size: 16px;
+  border-bottom: solid 1px;
+  width: 100%;
+  text-align: center;
+}
+
+.nav-items {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  font-size: 0.8em;
+}
 </style>
