8bed32e3a037e235ddaa9572e48525b17cefc5b6
michaels234 <mdsimeone@gmail.com>
2022-03-16 22:12:07 +0900

    android app, now i can copy!

--- Event Horizon Android Webview App/EventHorizon/app/src/main/java/com/example/videochatplatformwebviewandroidapp/MainActivity.kt
+++ Event Horizon Android Webview App/EventHorizon/app/src/main/java/com/example/videochatplatformwebviewandroidapp/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.example.EventHorizon
 
 import android.content.ClipData
-import android.content.Context
-import android.content.Context.CLIPBOARD_SERVICE
 import android.os.Bundle
 import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat.getSystemService
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,22 +14,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val webView: WebView = findViewById(R.id.webview)
         webView.webViewClient = WebViewClient()
-        webView.getSettings().setJavaScriptEnabled(true)
-        webView.addJavascriptInterface(WebAppInterface(), "NativeAndroid")
-        webView.getSettings().setLoadWithOverviewMode(true);
-        webView.getSettings().setUseWideViewPort(true);
-        webView.getSettings().setDisplayZoomControls(false);//Whether to use the built-in zoom mechanism
-        webView.getSettings().setSupportZoom(true);//  Whether to support zoom
+        webView.settings.javaScriptEnabled = true;
+        webView.addJavascriptInterface(WebAppInterface(this), "NativeAndroid")
+        webView.settings.loadWithOverviewMode = true;
+        webView.settings.useWideViewPort = true;
+        webView.settings.displayZoomControls = false;//Whether to use the built-in zoom mechanism
+        webView.settings.setSupportZoom(true);//  Whether to support zoom
         webView.setInitialScale(100);//  Zoom on initialization
         webView.loadUrl("https://event-horizon-test.herokuapp.com")
 //        webView.loadUrl("https://www.eventhorizon.vip")
     }
-}
-class WebAppInterface {
-  @JavascriptInterface
-  fun copyToClipboard(text: String?) {
-    val clipboard: android.content.ClipboardManager? = getSystemService<String>(CLIPBOARD_SERVICE) as android.content.ClipboardManager?
-    val clip = ClipData.newPlainText("demo", text)
-    clipboard.setPrimaryClip(clip)
-  }
+
+    class WebAppInterface(private val mainActivity: MainActivity) {
+      @JavascriptInterface
+      fun copyToClipboard(text: String?) {
+        val clipboard: android.content.ClipboardManager? = this.mainActivity.getSystemService(CLIPBOARD_SERVICE) as android.content.ClipboardManager?
+        val clip = ClipData.newPlainText("demo", text)
+        clipboard?.setPrimaryClip(clip)
+      }
+    }
 }
