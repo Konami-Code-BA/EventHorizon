ce4302a763fdb723a0bb970465db2171dffad73c
michaels234 <mdsimeone@gmail.com>
2022-03-16 19:48:40 +0900

    centered openeing page text for small devices. testing android copy button

--- Event Horizon Android Webview App/EventHorizon/app/src/main/java/com/example/videochatplatformwebviewandroidapp/MainActivity.kt
+++ Event Horizon Android Webview App/EventHorizon/app/src/main/java/com/example/videochatplatformwebviewandroidapp/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.EventHorizon
 
+import android.content.ClipData
+import android.content.Context
+import android.content.Context.CLIPBOARD_SERVICE
 import android.os.Bundle
+import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.getSystemService
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +18,7 @@ class MainActivity : AppCompatActivity() {
         val webView: WebView = findViewById(R.id.webview)
         webView.webViewClient = WebViewClient()
         webView.getSettings().setJavaScriptEnabled(true)
+        webView.addJavascriptInterface(WebAppInterface(), "NativeAndroid")
         webView.getSettings().setLoadWithOverviewMode(true);
         webView.getSettings().setUseWideViewPort(true);
         webView.getSettings().setDisplayZoomControls(false);//Whether to use the built-in zoom mechanism
@@ -21,4 +27,12 @@ class MainActivity : AppCompatActivity() {
         webView.loadUrl("https://event-horizon-test.herokuapp.com")
 //        webView.loadUrl("https://www.eventhorizon.vip")
     }
+}
+class WebAppInterface {
+  @JavascriptInterface
+  fun copyToClipboard(text: String?) {
+    val clipboard: android.content.ClipboardManager? = getSystemService<String>(CLIPBOARD_SERVICE) as android.content.ClipboardManager?
+    val clip = ClipData.newPlainText("demo", text)
+    clipboard.setPrimaryClip(clip)
+  }
 }
