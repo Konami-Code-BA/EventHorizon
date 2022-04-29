7aa57ad104351d72ba5b8fd824d7fd7925978bdf
michaels234 <mdsimeone@gmail.com>
2022-04-29 19:39:40 +0900

    login register change, massages change, header change, email input change, tabs padding change, settings page change,

--- src/views/settings.vue
+++ src/views/settings.vue
@@ -19,10 +19,7 @@
 			<div class="line-height"/>
 
 			<!-- E-mail preferences checkbox -->
-			<button class="button" v-if="store.user.email === ''" v-on:click.prevent="showAddEmailModal = true">
-				{{ t('ADD EMAIL ADDRESS') }}
-			</button>
-			<div v-else class="dual-set">
+			<div class="dual-set">
 				<button class="button" style="width: 100%;"
 						v-on:click.prevent="updateUserDoGetEmails()">
 					{{ t('GET EMAILS') }}&nbsp;
@@ -35,56 +32,8 @@
 
 			<div class="line-height"/>
 
-			<line-button :pageToReturnTo="currentPage" :wording="t('ADD LINE')" v-if="store.user.line_id === ''"
-					ref="lineButton"/>
-
-			
-			<div v-else class="dual-set">
-				<button class="button" style="width: 100%"
-						v-on:click.prevent="updateUserDoGetLines()">
-					{{ t('GET LINE MESSAGES') }}&nbsp;
-				</button>
-				<div class = "checkboxdiv">
-					<input type="checkbox" class="checkbox" v-model="store.user.do_get_lines"
-						:key="store.user.do_get_lines"/>
-				</div>
-			</div>
-
-			<div class="line-height"/>
-
-			<button class="button" v-if="store.user.password != ''" v-on:click.prevent="showChangePasswordModal = true">
-				{{ t('CHANGE PASSWORD') }}
-			</button>
 		</div>
 
-		<modal v-if="showAddEmailModal" @closeModals="showAddEmailModal = false" ref="showAddEmailModal">
-			<div slot="contents" class="modal">
-				<x-close-button :closeFunc="() => {$refs.showAddEmailModal.closeModals()}" style="align-self: flex-end;"/>
-				<form v-on:keyup.enter="addEmail()" style="width: 100%;">
-					<email-input ref="emailInput" usage="AddEmail"
-							:key="store.user.language+'emailInputAddEmail'"/>
-					<password-input ref="passwordInput" :doublePassword="true" usage="AddEmail"
-							:key="store.user.language+'passwordInputAddEmail'"/>
-				</form>
-				<button v-on:click.prevent="addEmail()" class="button" style="width: 100%;">
-					{{ t('ADD EMAIL ADDRESS') }}
-				</button>
-			</div>
-		</modal>
-		<modal v-if="showChangePasswordModal" @closeModals="showChangePasswordModal = false" ref="showChangePasswordModal">
-			<div slot="contents" class="modal">
-				<x-close-button :closeFunc="() => {$refs.showChangePasswordModal.closeModals()}" style="align-self: flex-end;"/>
-				<form v-on:keyup.enter="changePassword()" style="width: 100%;">
-					<password-input ref="passwordInput1" :doublePassword="false" usage="ChangePassword1"
-							customPlaceholder="CURRENT PASSWORD"/>
-					<password-input ref="passwordInput2" :doublePassword="true" usage="ChangePassword2"
-							customPlaceholder="NEW PASSWORD"/>
-				</form>
-				<button v-on:click.prevent="changePassword()" class="button" style="width: 100%">
-					{{ t('CHANGE PASSWORD') }}
-				</button>
-			</div>
-		</modal>
 		<!-- DISPLAY NAME CHANGE MODAL -->
 		<modal v-if="showChangeDisplayNameModal" @closeModals="showChangeDisplayNameModal = false" ref="showChangeDisplayNameModal">
 			<div slot="contents" class="modal">
@@ -96,7 +45,6 @@
 				</button>
         	</div>
 		</modal>
-		<flash-modal :text="t('DONE!')" ref="flashPasswordChangedSettings" :time="1000"/>
     	<flash-modal :text="t('DONE!')" ref="flashDisplayNameChanged" :time="1000"/>
 	</div>
 </template>
@@ -116,9 +64,6 @@
 		name: 'settings',
 		components: {
 			modal,
-			lineButton,
-			emailInput,
-			passwordInput,
 			flashModal,
 			displayNameInput,
 			xCloseButton,
@@ -126,13 +71,10 @@
 		data () {
 			return {
 				store: store,
-				showAddEmailModal: false,
-				showChangePasswordModal: false,
 				showChangeDisplayNameModal: false,
 			}
 		},
 		props: {
-			tryLine: { default: false },
 		},
 		computed: {
 			currentPage () {
@@ -140,91 +82,12 @@
 			}
 		},
 		async mounted () {
-			if (this.$refs.lineButton && this.tryLine) {
-				await this.$refs.lineButton.tryLineNewDevice()
-			}
-			if (this.showAddEmailModal) {
-				f.focusCursor(document, 'emailAddEmail')
-			}
 		},
 		methods: {
 			t (w) { return translations.t(w) },
 			async updateUserDoGetEmails () {
 				store.user.do_get_emails = !store.user.do_get_emails
 				await api.updateUserDoGetEmails()
-			},
-			async updateUserDoGetLines () {
-				store.user.do_get_lines = !store.user.do_get_lines
-				await api.updateUserDoGetLines()
-			},
-			async addEmail () {
-				this.$refs.passwordInput.hasErrors()
-				this.$refs.passwordInput.hasErrors2()
-				this.$refs.emailInput.hasErrors()
-				if (
-					this.$refs.passwordInput.error.length > 0
-					|| this.$refs.passwordInput.error2.length > 0
-					|| this.$refs.emailInput.error.length > 0
-				) {
-					f.shakeFunction([this.$refs.passwordInput, this.$refs.emailInput])
-					return
-				}
-				this.$refs.passwordInput.showPassword = false
-				this.$refs.passwordInput.showPassword2 = false
-				this.store.loading = true
-
-				let user = await api.addAnEmail(
-					this.$refs.emailInput.email,
-					this.$refs.passwordInput.password,
-				)
-				if (!user.error) {
-					f.goToPage(this.store.lastNonLoginRegisterPage)
-					this.showAddEmailModal = false
-					this.store.loading = false
-					return
-				}
-				if (user.error == 'Incorrect password for this email') {
-					this.$refs.passwordInput.error = user.error
-				}
-				this.store.loading = false
-				f.shakeFunction([this.$refs.passwordInput, this.$refs.emailInput])
-			},
-			async changePassword () {
-				this.$refs.passwordInput1.hasErrors()
-				this.$refs.passwordInput1.hasErrors2()
-				this.$refs.passwordInput2.hasErrors()
-				if (
-					this.$refs.passwordInput1.error.length > 0
-					|| this.$refs.passwordInput2.error2.length > 0
-				) {
-					f.shakeFunction([this.$refs.passwordInput1, this.$refs.passwordInput2])
-					return
-				}
-				this.$refs.passwordInput1.showPassword = false
-				this.$refs.passwordInput2.showPassword = false
-				this.$refs.passwordInput2.showPassword2 = false
-				this.store.loading = true
-
-				let user = await api.changePassword(
-					this.store.user.email,
-					this.$refs.passwordInput2.password,
-					null,
-					this.$refs.passwordInput1.password,
-				)
-				if (!user.error) {
-					this.showChangePasswordModal = false
-					this.$refs.passwordInput1.password = ''
-					this.$refs.passwordInput2.password = ''
-					this.$refs.passwordInput2.password2 = ''
-					this.store.loading = false
-					await this.$refs.flashPasswordChangedSettings.flashModal()
-					return
-				}
-				if (user.error == 'wrong code or password') {
-					this.$refs.passwordInput1.error = 'CURRENT PASSWORD IS INCORRECT'
-				}
-				this.store.loading = false
-				f.shakeFunction([this.$refs.passwordInput1, this.$refs.passwordInput2])
 			},
 			async changeDisplayName() {
 				this.$refs.displayNameInput.hasErrors()
